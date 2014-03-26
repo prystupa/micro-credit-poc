fa39d7c7da1cdeaf70ef40a5616f8a0c71ad3f23
prystupa <eugene.prystupa@gmail.com>
2014-03-26 13:26:03 -0400

    integrate cas-validate into node

--- web/client-web/app.js
+++ web/client-web/app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -8,24 +7,47 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
+var cas_validate = require('cas_validate');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+
+app.use(cas_validate.ticket({
+    'cas_host': 'localhost:8443',
+    'service': 'http://localhost:3000'
+}));
+app.use(cas_validate.check_or_redirect({
+    'cas_host': 'localhost:8443',
+    'service': 'http://localhost:3000'
+}));
+
+app.get('/username', function(req, res) {
+    var user = req.session.name;
+    res.end(user);
+});
+
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
